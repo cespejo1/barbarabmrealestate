fd4094e13461996ad17f34a1a2970b32bbc5b48a
Carlos Espejo <cespejo1@yahoo.com>
2026-09-22 14:40:27 -0400

    Add plain text sitemap fallback

--- app/robots.ts
+++ app/robots.ts
@@ -4,7 +4,7 @@ import { site } from './site-data';
 export default function robots(): MetadataRoute.Robots {
   return {
     rules: { userAgent: '*', allow: '/' },
-    sitemap: `${site.url}/sitemap.xml`,
+    sitemap: [`${site.url}/sitemap.xml`, `${site.url}/sitemap.txt`],
     host: site.url,
   };
 }
